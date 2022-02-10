9c4c6605f790f9df30a24b333899b259bd540039
Muhametii00 <konushevcimuhamet@gmail.com>
2022-02-10 16:23:29 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,42 +7,57 @@ const settings = [
     logo  : <img src={logo1} alt="logo"/>,
     title : <h3>The TLD</h3>,
     write : <p>Does the domain extension match the language of the domain name</p>
+  },
+  {
+    logo  : <img src={logo1} alt="logo"/>,
+    title : <h3>The TLD</h3>,
+    write : <p>Does the domain extension match the language of the domain name</p>
+  },
+  {
+    logo  : <img src={logo1} alt="logo"/>,
+    title : <h3>The TLD</h3>,
+    write : <p>Does the domain extension match the language of the domain name</p>
+  },
+  {
+    logo  : <img src={logo1} alt="logo"/>,
+    title : <h3>The TLD</h3>,
+    write : <p>Does the domain extension match the language of the domain name</p>
+  },
+  {
+    logo  : <img src={logo1} alt="logo"/>,
+    title : <h3>The TLD</h3>,
+    write : <p>Does the domain extension match the language of the domain name</p>
+  },
+  {
+    logo  : <img src={logo1} alt="logo"/>,
+    title : <h3>The TLD</h3>,
+    write : <p>Does the domain extension match the language of the domain name</p>
+  },
+  {
+    logo  : <img src={logo1} alt="logo"/>,
+    title : <h3>The TLD</h3>,
+    write : <p>Does the domain extension match the language of the domain name</p>
+  },
+  {
+    logo  : <img src={logo1} alt="logo"/>,
+    title : <h3>The TLD</h3>,
+      write : <p>Does the domain extension match the language of the domain name</p>
+  },
+  {
+    logo  : <img src={logo1} alt="logo"/>,
+    title : <h3>The TLD</h3>,
+      write : <p>Does the domain extension match the language of the domain name</p>
   }
 ]
 
 export default function App() {
     return(
-        <div>
+        <div class='box'>
             {settings.map((setting) => {
               const { title, write, logo} = setting;
-              return <div class="box">
-              <div className='one'>{logo}
+              return <div className='one'>
+              <span>{logo}</span>
               <li>{title}{write}</li>
-              </div>
-              <div className='two'>{logo}
-              <li>{title}{write}</li>
-              </div>
-              <div className='three'>{logo}
-              <li>{title}{write}</li>
-              </div>
-              <div className='four'>{logo}
-              <li>{title}{write}</li>
-              </div>
-              <div className='five'>{logo}
-              <li>{title}{write}</li>
-              </div>
-              <div className='six'>{logo}
-              <li>{title}{write}</li>
-              </div>
-              <div className='seven'>{logo}
-              <li>{title}{write}</li>
-              </div>
-              <div className='eight'>{logo}
-              <li>{title}{write}</li>
-              </div>
-              <div className='nine'>{logo}
-              <li>{title}{write}</li>
-              </div>
             </div>
           })}
         </div>
